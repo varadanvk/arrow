988abec8752a8d9d824e2fa9a9d464c527e52e7e
varadanvk <varadan@varadans-MacBook-Pro.local>
2025-08-05 15:26:25 -0700

    /

--- arrow.rb
+++ arrow.rb
@@ -1,5 +1,5 @@
 class Arrow < Formula
-    desc "Vector database with persistent storage built in Rust"
+    desc "Vector database with persistent storage built in Rust!"
     homepage "https://github.com/varadanvk/arrow"
     url "https://github.com/varadanvk/arrow/archive/refs/tags/0.0.2.tar.gz"
     sha256 "7a100495e92d575afb1954b01899c7252a9719f5323e7e7d945b1ef4a831a8d9"
